572963de7cd0d1c75a9a45c8f8e5725cbe47d711
plcmark2 <plc.pabloleiva@gmail.com>
2024-04-25 22:44:23 -0400

    fix consumer

--- agente.js
+++ agente.js
@@ -3,18 +3,34 @@ import { obtenerAgentePorId } from "./utils.js";
 const render = async () => {
     const params = new URLSearchParams(window.location.search);
 
-    const id= params.get ("id");
+    const id= params.get("id");
 
-    const agente = await obtenerAgentePorId(parseInt(id));  
+    console.log("ID :: ", id);
+
+    const agente = await obtenerAgentePorId(id);  
     
+    console.log(agente);
+
     const container = document.querySelector(".agenteMain");
     
     const containerImg = document.createElement("img");
-    containerImg.src = personaje.fullPortrait;
-    containerImg.alt = personaje.displayName;
-
-
+    containerImg.src = agente.fullPortrait == null ? agente.displayIcon : agente.fullPortrait;
+    containerImg.alt = agente.displayName;
+    containerImg.classList.add("agente__imagen");
+ 
     container.appendChild(containerImg);
+
+    const spanName = document.createElement("span");
+    spanName.textContent = agente?.displayName;
+    container.appendChild(spanName);
+
+    const pDescription = document.createElement("p");
+    pDescription.textContent = agente?.description;
+    container.appendChild(pDescription);
+
+    const RolDescription = document.createElement("p");
+    RolDescription.textContent = agente?.rol?.description;
+    container.appendChild(RolDescription);
 
     return container;
 };
